1e6159dd1502d05b1ba4252e14e8295e70d6d9ee
Kamil Erguler <kerguler@gmail.com>
2021-01-07 00:20:43 +0200

    ex4 (working with quant)

--- docs/examples/ex4_life_cycle/ex4_life_cycle.c
+++ docs/examples/ex4_life_cycle/ex4_life_cycle.c
@@ -1,51 +1,55 @@
 #include "spop2/spop2.h"
 
-void print_out(unsigned int tm, spop *egg, spop *larva, spop *pupa, spop *adult) {
+void print_out(unsigned int tm, quant *egg, quant *larva, quant *pupa, quant *adult) {
     printf("%d,%g,%g,%g,%g\n",tm,(*egg)->size.d,(*larva)->size.d,(*pupa)->size.d,(*adult)->size.d);
 }
 
 int main(void) {
     unsigned char mode = MODE_ACCP_ERLANG;
     unsigned int tm = 0;
+    sdnum tmp;
     //
-    set_APPROX(1e-3);
+    // set_APPROX(1e-2);
     //
-    spop egg = spop_init(0, mode);
-    spop larva = spop_init(0, mode);
-    spop pupa = spop_init(0, mode);
-    spop adult = spop_init(0, mode);
+    quant egg = quant_init(0, mode);
+    quant larva = quant_init(0, mode);
+    quant pupa = quant_init(0, mode);
+    quant adult = quant_init(0, mode);
     //
-    spop_add(egg, 0, 0, 0, 0, 100);
+    quant_add(egg, 0, 100);
     print_out(tm, &egg, &larva, &pupa, &adult);
     //
-    for (tm=1; tm<40; tm++) {
-        spop_iterate(egg,  0, 10, 1, 0,  0, 0, 0, 0,  0);
-        spop_iterate(larva,  0, 10, 1, 0,  0, 0, 0, 0,  0);
-        spop_iterate(pupa,  0, 10, 1, 0,  0, 0, 0, 0,  0);
-        spop_iterate(adult,  0, 10, 1, 0,  0, 0, 0, 0,  0);
+    for (tm=1; tm<100; tm++) {
+        quant_iterate(egg,   10, 1);
+        quant_iterate(larva, 10, 1);
+        quant_iterate(pupa,  10, 1);
+        quant_iterate(adult, 10, 1);
         //
-        spop_add(egg, 0, 0, 0, 0, 1.0 * adult->size.d);
-        spop_popadd(larva, egg, 1);
-        spop_popadd(pupa, larva, 1);
+        quant_add(egg, 0, 1.0 * adult->size.d);
+        quant_sdpopadd(larva, egg, 1, &tmp);
+        quant_sdpopadd(pupa, larva, 1, &tmp);
         if (tm==20) {
-            spop_print(pupa->devtable);
+            //printf("\n");
+            //quant_print(pupa);
         }
-        spop_iterate(pupa->devtable,  0, 0, 0, 0,  0.5, 0, 0, 0,  1);
+        quant_survive(pupa,  0.5, 1, &tmp);
         if (tm==20) {
-            spop_print(pupa->devtable);
-            spop_print(adult);
+            //quant_print(pupa);
+            //printf("\n");
+            //quant_print(adult);
         }
-        spop_popadd(adult, pupa->devtable, 0);
+        quant_sdpopadd(adult, pupa, 1, &tmp);
         if (tm==20) {
-            spop_print(adult);
+            //quant_print(adult);
+            //printf("\n");
         }
         //
         print_out(tm, &egg, &larva, &pupa, &adult);
     }
     //
-    spop_destroy(&egg);
-    spop_destroy(&larva);
-    spop_destroy(&pupa);
-    spop_destroy(&adult);
+    quant_destroy(&egg);
+    quant_destroy(&larva);
+    quant_destroy(&pupa);
+    quant_destroy(&adult);
     return 0;
 }
